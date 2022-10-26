09c264a5c6d243d41a6cc159ea5a88932ba81794
linyuelin <linyuelin1997@gmail.com>
2022-10-26 23:02:52 +0800

    no message

---
+++ Comment01.java
@@ -0,0 +1,14 @@
+//注釈の説明
+public class Comment01 {
+	
+	//主な内容を編集する
+	public static void main(String[] args) {
+		
+	    int n1 =10;
+	    int n2 =30;
+	    int sum=n1+n2;
+	    //結果の輸出
+	    System.out.println("结果="+sum);
+	   
+	}
+}
---
+++ Comment02.java
@@ -0,0 +1,15 @@
+
+/**
+*@author lin
+*@version 1.o
+*/
+public class Comment02 {
+	/*
+		これは”類”という物です
+		以下です			
+		*/
+	public static void main (String[] args) {
+		System.out.println("毎日充実です");
+		
+	}
+}
